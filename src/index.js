6e6922a4b31ec1b5b3e5f64e07958e25d9f5c32b
Abel M. Osorio <abel.m.osorio@gmail.com>
2020-01-22 14:17:08 -0300

    Create type Person, query people and mutation createPerson.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,56 @@ import gql from 'graphql-tag'
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    people: [Person]
+  }
+
+  type Mutation {
+    createPerson(firstName: String!, lastName: String!): Person
+  }
+
+  type Person {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    parent: Person
+    age: Int
   }
 `
 
+const db = {
+  people: [
+    { id: 1, firstName: 'Abel', lastName: 'Osorio', parentId: 10, birthDate: '1989-04-07' },
+    { id: 2, firstName: 'Marcela', lastName: 'Osorio', parentId: 10 },
+    { id: 10, firstName: 'Mauri', lastName: 'Osorio', parentId: 4 },
+    { id: 4, firstName: 'Eze', lastName: 'Osorio', parentId: null }
+  ]
+}
+
+const getPeople = () => {
+  return db.people
+}
+
+const createPerson = (person) => {
+  db.people.push(person)
+
+  return person
+}
+
+const getPersonById = (id) => {
+  if (!id) return undefined
+
+  return db.people.find((person) => person.id === id)
+}
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    people: () => getPeople()
+  },
+  Mutation: {
+    createPerson: (_, input) => createPerson(input)
+  },
+  Person: {
+    parent: (person) => getPersonById(person.parentId)
   }
 }
 
